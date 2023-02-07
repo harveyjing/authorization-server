7f22a73dd4cd0bd2ec99f1ed698250c4a5afbe79
William f Jing <william.f.jing@cn.pwc.com>
2023-02-07 14:31:00 +0800

    init

---
+++ src/test/java/org/personal/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package org.personal.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
